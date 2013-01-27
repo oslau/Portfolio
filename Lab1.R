485af18ed0fd52ec31e09859c0badfe5bc6813ed
Olivia Lau <oliviaslau@gmail.com>
2013-01-26 22:45:48 -0800

    232 Lab1 v2

--- Lab1.R
+++ Lab1.R
@@ -19,44 +19,45 @@ dat = groupedData(Obs ~ Sire|Line, data = dat)
 #ML and REML Estimates
 ml = lme(Obs ~ Line + x1 + x2 - 1, random = ~1|Sire, data = dat, method = "ML")
 reml = lme(Obs ~ Line + x1 + x2 - 1, random = ~1|Sire, data = dat)
-ml$sigma #error sd
-diag(sqrt(getVarCov(ml)))	#random effect sd
-reml$sigma	#error sd
-diag(sqrt(getVarCov(reml)))[1]	#random effect sd
-#summary(ml)
-#summary(reml)
-a1 = fixef(ml)[1]
-a2 = fixef(ml)[2]
-ranef(ml)
-fixef(reml)
-ranef(reml)
 
-n = nrow(dat)
-V = matrix(c())
+summary(ml)
+summary(reml)
 
-gen.LMM = function(model, dat){
-	X = cbind(as.numeric(dat[,"Line"] == 1), 
-		as.numeric(dat[,"Line"] == 2),	
-		as.numeric(dat[,"Line"] == 3), 
-		as.numeric(dat[,"Line"] == 4), 
+gen.LMM = function(model, dat, nboot){
+	sd.e = model$sigma #error sd
+	sd.s = diag(sqrt(getVarCov(model)))	#random effect sd
+	
+	X = cbind(as.numeric(dat[,"Line"] == 4), 
+		as.numeric(dat[,"Line"] == 1),	
 		as.numeric(dat[,"Line"] == 5), 
+		as.numeric(dat[,"Line"] == 3), 
+		as.numeric(dat[,"Line"] == 2), 
 		dat$x1, dat$x2)
-	B = c(l = ranef(model)[,1], fixef(model))
+	B = fixef(model)
+	
 	temp = table(dat$Sire)
+	d = length(temp)
+	n = nrow(dat)
 	Z = sapply(1:length(temp), function(i){
 		c(rep(0,sum(temp[1:i-1])), rep(1,temp[i]), rep(0, n-sum(temp[1:i])))
 	})
-	s = as.matrix(unique(dat$Sire))
-	y.LMM = X %*% B + Z %*% s
-	dat = cbind(Obs = y.LMM, dat[,-1])
+	G = diag(sd.s, nrow = d, ncol = d)
+	R = diag(sd.e, nrow = n, ncol = n)
+	V = R + Z %*% G %*% t(Z)
+	
+	mvrnorm(nboot, X %*% B, V)
 	return(dat)
 }
 
 #Bootstrap
-B = 100
+n.boot = 100
 
 #Initialize
-newdat = gen.LMM(ml, dat)
+dat.boot.ml = gen.LMM(ml, dat, n.boot)
+dat.boot.reml = gen.LMM(reml, dat, n.boot)
+
+
+
 bootst = function(B, model = "REML", newdat){
 	sapply(1:B, function(i){
 		fit = lme(Obs ~ x1 + x2 - 1, random = ~1|Sire|Line, data = newdat, method = model)
