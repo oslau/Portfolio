29e7fb9f0a62bacba49304c5769aece472a9fbb5
Olivia Lau <oliviaslau@gmail.com>
2013-01-27 15:02:37 -0800

    Testing subversion 1

--- BML Traffic.R
+++ BML Traffic.R
@@ -8,30 +8,38 @@ grid.r = 20 ##number of rows
 grid.c = 10 ##number of columns
 rho = .5	##proportion of grid filled
 
+##################
+###SIMPLE CALCS###
+##################
+ncars = round(rho * grid.r * grid.c, 0)	##total number of cars
+totcells = grid.r * grid.c	##total number of cells
+
 ##############################
 ###GENERATE RANDOM CAR GRID###
 ##############################
-##total number of cars to the nearest integer
-ncars = round(rho * grid.r * grid.c, 0)
-totcells = grid.r * grid.c
-
-possible.y = rep(1:grid.r, grid.c)
-possible.x = rep(1:grid.c, each = grid.r)
-possible.coords = cbind(possible.y, possible.x)
-
-samp = sample(totcells, ncars)
-samp.coords = possible.coords[samp,]
-colnames(samp.coords)  = c("y", "x")
-colors = sample(c("blue", "red"), ncars, replace = TRUE)
-
-#############
-###TESTING###
-#############
 
 colors = sample(c("blue", "red"), totcells, replace = TRUE)
 colors = matrix(colors, nrow = grid.r)
 possible = melt(colors)
-names(possible) = c("rows", "cols", "color")
+names(possible) = c("y", "x", "color")
 samp = sample(totcells, ncars)
 mysample = possible[samp, ]
-qplot(cols, rows, fill = color, data = mysample, geom = 'tile')
+qplot(x, y, fill = color, data = mysample, geom = 'tile') + scale_fill_manual(name = "Car Types", values = c("blue", "red"), labels = c("blue - up", "red - right"))
+
+#############
+###TESTING###
+#############
+#t = 1, blue moves up ALL AT ONCE
+blue = subset(mysample, color == "blue")
+##current position
+curr.pos.x = blue[1, "x"]
+curr.pos.y = blue[1, "y"]
+##check if moving is possible
+check = any(mysample[,"x"] == curr.pos.x & mysample[,"y"] == (curr.pos.y - 1)) 
+if(check == TRUE){
+	break
+}
+if(check == FALSE){
+	curr.pos.y = blue[1, "y"] - 1
+	change = change + 1
+}
