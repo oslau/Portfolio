1f2b1a5498dd15071866a9332a6d66cdb9b6f86c
Olivia Lau <oliviaslau@gmail.com>
2013-01-28 06:33:51 -0800

    Update subversion

--- BML Traffic.R
+++ BML Traffic.R
@@ -18,7 +18,7 @@ totcells = grid.r * grid.c	##total number of cells
 ###GENERATE RANDOM CAR GRID###
 ##############################
 
-colors = sample(c("blue", "red"), totcells, replace = TRUE)
+colors = sample(c(1, 2), totcells, replace = TRUE)
 colors = matrix(colors, nrow = grid.r)
 possible = melt(colors)
 names(possible) = c("y", "x", "color")
@@ -29,17 +29,70 @@ qplot(x, y, fill = color, data = mysample, geom = 'tile') + scale_fill_manual(na
 #############
 ###TESTING###
 #############
-#t = 1, blue moves up ALL AT ONCE
-blue = subset(mysample, color == "blue")
-##current position
-curr.pos.x = blue[1, "x"]
-curr.pos.y = blue[1, "y"]
-##check if moving is possible
-check = any(mysample[,"x"] == curr.pos.x & mysample[,"y"] == (curr.pos.y - 1)) 
-if(check == TRUE){
-	break
+test.move = function(coords, dir){
+	switch(dir,
+		up = c(x = coords[ , "x"], y = (coords[ , "y"]+1)),
+		down = c(x = coords[ , "x"], y = (coords[ , "y"]-1)),
+		left = c(x =(coords[ , "x"] + 1), y = (coords[ , "y"])),
+		right = c(x = (coords[ , "x"] - 1), y = coords[ , "y"])
+	)
 }
-if(check == FALSE){
-	curr.pos.y = blue[1, "y"] - 1
-	change = change + 1
+
+# move = function(i, dat){
+# 	curr.pos.x = mysample[i, "x"]
+# 	curr.pos.y = mysample[i, "y"]
+# 	potential = curr.pos.y + 1
+# 	if(potential > grid.r){		##check if potential move is off grid
+# 		potential = 1
+# 	}
+# 	##check if moving is possible
+# 	check = any(mysample[,"x"] == curr.pos.x & mysample[,"y"] == potential)
+# 	##print(check)
+# 	if(check == FALSE){
+# 		mysample[i, "y"] = potential
+# 		#change = change + 1
+# 	}
+# 	list(mysample[i,"y"], mysample[i,"x"])
+# }
+
+
+##More generic function...##
+move = function(i, dat, direction){
+	pos.x = mysample[i, "x"]
+	pos.y = mysample[i, "y"]
+	next.pos = test.move(c(x = pos.x, y = pos.y), direction)
+	
+	if(next.pos[,"y"] > grid.r){	##check if potential move is off grid
+		next.pos[,"y"] = 1
+	}
+	else{
+		if(next.pos[,"y"] < 1){
+			next.pos[,"y"] = grid.r
+		}
+	}
+	if(next.pos[,"x"] > grid.c){
+		next.pos[,"x"] = 1
+	}
+	else{
+		if(next.pos[,"x"] < 1){
+			next.pos[,"x"] = grid.c
+		}	
+	}
+	
+	##check if moving is possible
+	check = any(mysample[,"x"] == curr.pos.x & mysample[,"y"] == potential)
+	##print(check)
+	if(check == FALSE){
+		mysample[i, "y"] = potential
+		#change = change + 1
+	}
+	return(mysample[i,])
 }
+
+blue = which(mysample$color == "blue")
+##FIX THIS! -- doesn't work because it coerces into a matrix
+##apply(blue, 1, move, dat = mysample)
+data.frame(sapply(blue, move, dat = mysample))
+red = which(mysample$color == "red")
+data.frame(sapply(blue, move, dat = mysample))
+
