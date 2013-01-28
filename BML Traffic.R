22baae98f17ec21d0e4f3278eeacb77062562973
Olivia Lau <oliviaslau@gmail.com>
2013-01-28 10:28:13 -0800

    Refined movement functions

--- BML Traffic.R
+++ BML Traffic.R
@@ -18,81 +18,71 @@ totcells = grid.r * grid.c	##total number of cells
 ###GENERATE RANDOM CAR GRID###
 ##############################
 
-colors = sample(c(1, 2), totcells, replace = TRUE)
-colors = matrix(colors, nrow = grid.r)
+colors = matrix(sample(c(1, 2), totcells, replace = TRUE), nrow = grid.r)
 possible = melt(colors)
 names(possible) = c("y", "x", "color")
 samp = sample(totcells, ncars)
 mysample = possible[samp, ]
-qplot(x, y, fill = color, data = mysample, geom = 'tile') + scale_fill_manual(name = "Car Types", values = c("blue", "red"), labels = c("blue - up", "red - right"))
+qplot(x, y, fill = as.factor(color), data = mysample, geom = 'tile') + scale_fill_manual(name = "Car Types", values = c("blue", "red"), labels = c("blue - up", "red - right"))
+
+####################
+###MOVE THAT CAR!###
+####################
+
+##This function changes the coordinates according to specified direction
+next.move = function(coords, dir){
+	switch(dir,
+		up = cbind(y = (coords[,"y"] + 1), x = coords[,"x"], color = coords[,"color"]),
+		down = cbind(y = (coords[,"y"] - 1), x = coords[,"x"], color = coords[,"color"]),
+		left = cbind(y = coords[,"y"], x =(coords[,"x"] - 1), color = coords[,"color"]),
+		right = cbind(y = coords[,"y"], x = (coords[,"x"] + 1), color = coords[,"color"])
+	)
+}
+
+##This function checks if the move generated aboce is off the grid
+##If so, it will wrap around.
+off.grid = function(coords){
+	if(coords[,"y"] > grid.r){	
+		coords[,"y"] = 1
+	}
+	else{
+		if(coords[,"y"] < 1){
+			coords[,"y"] = grid.r
+		}
+	}
+	if(coords[,"x"] > grid.c){
+		coords[,"x"] = 1
+	}
+	else{
+		if(coords[,"x"] < 1){
+			coords[,"x"] = grid.c
+		}	
+	}
+	return(coords)
+}
+
+##This function implements the car movement at time t
+move = function(i, dat, direction){
+	curr.pos = dat[i,]
+	next.pos = next.move(curr.pos, direction)
+	next.pos = off.grid(next.pos)
+	check = any(dat[,"x"] == next.pos[,"x"] & dat[,"y"] == next.pos[,"y"])
+	if(check == FALSE){
+		curr.pos = next.pos
+	}
+	return(curr.pos)
+}
 
 #############
 ###TESTING###
 #############
-test.move = function(coords, dir){
-	switch(dir,
-		up = c(x = coords[ , "x"], y = (coords[ , "y"]+1)),
-		down = c(x = coords[ , "x"], y = (coords[ , "y"]-1)),
-		left = c(x =(coords[ , "x"] + 1), y = (coords[ , "y"])),
-		right = c(x = (coords[ , "x"] - 1), y = coords[ , "y"])
-	)
-}
 
-# move = function(i, dat){
-# 	curr.pos.x = mysample[i, "x"]
-# 	curr.pos.y = mysample[i, "y"]
-# 	potential = curr.pos.y + 1
-# 	if(potential > grid.r){		##check if potential move is off grid
-# 		potential = 1
-# 	}
-# 	##check if moving is possible
-# 	check = any(mysample[,"x"] == curr.pos.x & mysample[,"y"] == potential)
-# 	##print(check)
-# 	if(check == FALSE){
-# 		mysample[i, "y"] = potential
-# 		#change = change + 1
-# 	}
-# 	list(mysample[i,"y"], mysample[i,"x"])
-# }
+blue = which(mysample$color == 1)
+t(sapply(blue, move, dat = mysample, direction = "up"))
+red = which(mysample$color == 2)
+t(sapply(red, move, dat = mysample, direction = "right"))
 
-
-##More generic function...##
-move = function(i, dat, direction){
-	pos.x = mysample[i, "x"]
-	pos.y = mysample[i, "y"]
-	next.pos = test.move(c(x = pos.x, y = pos.y), direction)
-	
-	if(next.pos[,"y"] > grid.r){	##check if potential move is off grid
-		next.pos[,"y"] = 1
-	}
-	else{
-		if(next.pos[,"y"] < 1){
-			next.pos[,"y"] = grid.r
-		}
-	}
-	if(next.pos[,"x"] > grid.c){
-		next.pos[,"x"] = 1
-	}
-	else{
-		if(next.pos[,"x"] < 1){
-			next.pos[,"x"] = grid.c
-		}	
-	}
-	
-	##check if moving is possible
-	check = any(mysample[,"x"] == curr.pos.x & mysample[,"y"] == potential)
-	##print(check)
-	if(check == FALSE){
-		mysample[i, "y"] = potential
-		#change = change + 1
-	}
-	return(mysample[i,])
-}
-
-blue = which(mysample$color == "blue")
-##FIX THIS! -- doesn't work because it coerces into a matrix
-##apply(blue, 1, move, dat = mysample)
-data.frame(sapply(blue, move, dat = mysample))
-red = which(mysample$color == "red")
-data.frame(sapply(blue, move, dat = mysample))
-
+##Things to do:
+##Replace old values for new values
+##Implement for time=t iterations
+##
